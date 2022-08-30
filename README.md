bc175e29c987a36443ea93948e675636df43288e
Muhammad Mujtaba <mujtabax17@gmail.com>
2022-08-30 21:02:04 +0500

    Adding when Coding is Donet

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-# dice_flutter
+# Dice Flutter
+It is my 3<sup>rd</sup> flutter app while learning the flutter.
 
-It is a Dice app in which we learn the statefull widgets. it will roll dice which clicked
+## Functionality:
 
-## Getting Started
+It generate the Random pair of Dice Numbers while clicking on it.
 
+## Learning 
 
+I have learned and Used in this Project
+
+1.  Statefull Widget
+
+2.  Variable
+
+3.  Dart:math
+
+4.  Random
+
+5.  Functions
+
+6.  Text Button
+
+7.  Expanded
+
+## Final Screenshot
+
+<img src="images/project_ss.png" style="width:2.96722in;height:5.07657in" />
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,62 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: const Text('Dice'),
+          backgroundColor: Colors.teal,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int updateLeftImage=1;
+  int updateRightImage=1;
+
+  void changeDiceImage(){
+    setState((){
+    updateLeftImage= Random().nextInt(6)+1;
+    updateRightImage= Random().nextInt(6)+1;
+  });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: TextButton(
+
+              onPressed: ()
+              {
+                changeDiceImage();
+              },
+              child: Image.asset("images/dice$updateLeftImage.png"))
+          ),
+          Expanded(child: TextButton(
+              onPressed: ()
+              {
+                changeDiceImage();
+              },
+              child: Image.asset("images/dice$updateRightImage.png"))
+          ),
+        ],
+      ),
+    );
   }
 }
